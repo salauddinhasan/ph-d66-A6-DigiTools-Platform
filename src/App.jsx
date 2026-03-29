4668a95a3bfb7d667a4144338bba04ff0be49f1e
salauddin <salauddin@hasan.com>
2026-03-29 22:01:31 +0600

    feat: add Tailwind CSS and DaisyUI setup with basic UI test

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+ 
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <div className='p-8'>
+       <h1 className='text-3xl font-bold text-center'>Check tailwind</h1>
+       <div className=' flex justify-center p-5'>
+        <button className='btn btn-primary '>Check daisyUi</button>
+       </div>
+    </div>
+  )
+}
+
+export default App
